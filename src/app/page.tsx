093847b7a3161819ac236a7cce6f87b061fe0458
rjabhi <rjabhi@amazon.com>
2024-12-13 08:18:12 -0800

    update to gen2 outputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import config from './amplifyconfiguration.json'
+import config from './amplify_outputs.json'
 Amplify.configure(config);
 
 function App({ signOut, user }: WithAuthenticatorProps) {
